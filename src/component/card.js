0f21b125d15d240dfdfdbefbd1e17be0ed5f4f77
Indutnj <122307911+Indutnj@users.noreply.github.com>
2023-03-20 17:54:09 +0530

    Update card.js

--- src/component/card.js
+++ src/component/card.js
@@ -1,25 +1,25 @@
  import React from "react";
  export default function Card(props){
     const { price}=props;
-    return <div class="col-lg-4">
-    <div class="card mb-5 mb-lg-0">
-      <div class="card-body">
-        <h5 class="card-title text-muted text-uppercase text-center">Plus</h5>
-        <h6 class="card-price text-center">${price}<span class="period">/month</span></h6>
+    return <div className="col-lg-4">
+    <div className="card mb-5 mb-lg-0">
+      <div className="card-body">
+        <h5 className="card-title text-muted text-uppercase text-center">Plus</h5>
+        <h6 className="card-price text-center">${price}<span className="period">/month</span></h6>
         <hr/>
-        <ul class="fa-ul">
-          <li><span class="fa-li"><i class="fas fa-check"></i></span><strong>5 Users</strong></li>
-          <li><span class="fa-li"><i class="fas fa-check"></i></span>50GB Storage</li>
-          <li><span class="fa-li"><i class="fas fa-check"></i></span>Unlimited Public Projects</li>
-          <li><span class="fa-li"><i class="fas fa-check"></i></span>Community Access</li>
-          <li><span class="fa-li"><i class="fas fa-check"></i></span>Unlimited Private Projects</li>
-          <li><span class="fa-li"><i class="fas fa-check"></i></span>Dedicated Phone Support</li>
-          <li><span class="fa-li"><i class="fas fa-check"></i></span>Free Subdomain</li>
-          <li class="text-muted"><span class="fa-li"><i class="fas fa-times"></i></span>Monthly Status
+        <ul className="fa-ul">
+          <li><span className="fa-li"><i className="fas fa-check"></i></span><strong>5 Users</strong></li>
+          <li><span className="fa-li"><i className="fas fa-check"></i></span>50GB Storage</li>
+          <li><span className="fa-li"><i className="fas fa-check"></i></span>Unlimited Public Projects</li>
+          <li><span className="fa-li"><i className="fas fa-check"></i></span>Community Access</li>
+          <li><span className="fa-li"><i className="fas fa-check"></i></span>Unlimited Private Projects</li>
+          <li><span className="fa-li"><i className="fas fa-check"></i></span>Dedicated Phone Support</li>
+          <li><span className="fa-li"><i className="fas fa-check"></i></span>Free Subdomain</li>
+          <li className="text-muted"><span className="fa-li"><i className="fas fa-times"></i></span>Monthly Status
             Reports</li>
         </ul>
-        <div class="d-grid">
-          <a href="#" class="btn btn-primary text-uppercase">Button</a>
+        <div className="d-grid">
+          <a href="#" className="btn btn-primary text-uppercase">Button</a>
         </div>
       </div>
     </div>
